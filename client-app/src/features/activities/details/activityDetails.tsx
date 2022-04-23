e65bebe0e9cee4941f10740fc1bdbc70a287ab69
Rodrigo De Souza Silva <neidianerodrigo@terra.com.br>
2022-04-23 13:40:51 -0300

    final da seção 4

---
+++ client-app/src/features/activities/details/activityDetails.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Card, Image } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activity: Activity;
+    cancelSelectActivity: () => void;
+    openForm: (id: string)=>void;
+}
+export default function ActivityDetails({ activity, cancelSelectActivity, openForm }: Props) {
+    return (
+        <Card>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{activity.title}</Card.Header>
+                <Card.Meta>
+                    {activity.date}
+                </Card.Meta>
+                <Card.Description>
+                    {activity.description}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group>
+                    <Button onClick={()=>openForm(activity.id)} basic color="blue" content='Edit' />
+                    <Button onClick={cancelSelectActivity} basic color="grey" content='Cancel' />
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
